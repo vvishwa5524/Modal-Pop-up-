68f4f5d7c8b97679395b83bb7af75b9c425c1b08
Tuduru Vishwanath <151444699+vvishwa5524@users.noreply.github.com>
2024-12-26 21:47:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const CloseModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+const OpenModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for(let i=0;i<btnsOpenModal.length;i++)
+   btnsOpenModal[i].addEventListener('click',OpenModal);
+
+closeModal.addEventListener('click',CloseModal);
+overlay.addEventListener('click',CloseModal);
+
+document.addEventListener('keydown',function(e){
+ if(e.key==='Escape' && !modal.classList.contains('hidden')){
+  CloseModal();
+ }
+});
